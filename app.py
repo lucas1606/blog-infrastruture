609ede28f46624b53861a308720371d4d6d8f72a
lucas1606 <lucas.almeida1606@gmail.com>
2021-08-09 20:00:58 -0300

    Added route to show post

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html',post_list = post_list)
 
+@app.route("post/<int:post_id>")
+def show_post(post_id):
+    
+    return render_template('post.html', post = post_list[post_id])
+        
+
 @app.route("/login")
 def login(user, password):
 
