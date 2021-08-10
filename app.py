4aa8478826125790ad0f1d93945e6a925437299c
lucas1606 <lucas.almeida1606@gmail.com>
2021-08-10 19:21:36 -0300

    Created a popup login form for the admin user

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from posts import post_list
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home_page():
     return render_template('home.html',post_list = post_list)
 
-@app.route("post/<int:post_id>")
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
-    
     return render_template('post.html', post = post_list[post_id])
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
         
-
-@app.route("/login")
-def login(user, password):
-
-    pass
-
 
 if __name__ == '__main__':   
     app.run(debug= True)
